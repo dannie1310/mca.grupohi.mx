63524c6b7a9f3f1a4f2b624b98ba1619993d348e
JLopezA <gsus.arriaga@gmail.com>
2017-07-12 16:35:22 -0500

    Validacion de busqueda de datos de Tickets. Complemento nav bar

--- resources/views/partials/nav-app.blade.php
+++ resources/views/partials/nav-app.blade.php
@@ -150,6 +150,24 @@
   </li>
   @endif
 
+  @if(Auth::user()->hasRole(['administrador-permisos','auditoria','administrador-sistema']))
+
+      <li class="dropdown">
+          <a tabindex="0" href="#" class="dropdown-toggle" data-toggle="dropdown" data-submenu>
+              Administración<span class="caret"></span>
+          </a>
+          <ul class="dropdown-menu" role="menu">
+              @if(Auth::user()->hasRole(['administrador-permisos','administrador-sistema']))
+                  <li><a href="{{ route('administracion.roles_permisos') }}">Configuración general</a></li>
+              @endif
+              <li><a href="{{ route('detalle.configuracion') }}">Detalle configuración</a></li>
+          <!-- <li><a href="{{ route('usuarios_sistema.index') }}">Alta de usuarios</a></li> -->
+              @if(Auth::user()->hasRole(['administrador-permisos','administrador-sistema']))
+                  <li><a href="{{ route('usuario_proyecto.index') }}">Asignación de Usuarios a Proyectos</a></li>
+              @endif
+          </ul>
+      </li>
+  @endif
 
 @else
   <li><a href="{{ route('proyectos') }}">Proyectos</a></li>
