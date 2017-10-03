894795baa345be0042f1a81726d903d75405aa1f
DBENITEZ <DBENITEZ@DBenitezC>
2017-10-03 18:37:07 -0500

    validar rol y permiso para el modulo de administracion

--- resources/views/partials/nav-app.blade.php
+++ resources/views/partials/nav-app.blade.php
@@ -249,14 +249,14 @@
           </ul>
       </li>
   @endif
-  @if(Auth::user()->hasRole(['administrador-permisos','auditoria','administrador-sistema','auditoria-resumen-configuracion']))
+  @if(Auth::user()->hasRole(['administrador-permisos','auditoria','administrador-sistema']))
 
       <li class="dropdown">
           <a tabindex="0" href="#" class="dropdown-toggle" data-toggle="dropdown" data-submenu>
               Administración<span class="caret"></span>
           </a>
           <ul class="dropdown-menu" role="menu">
-              @if(Auth::user()->hasRole(['administrador-permisos','administrador-sistema','auditoria-resumen-configuracion']))
+              @if(Auth::user()->hasRole(['administrador-permisos','administrador-sistema','auditoria']))
                   <li><a href="{{ route('administracion.roles_permisos') }}">Configuración general</a></li>
               @endif
               <li><a href="{{ route('detalle.configuracion') }}">Detalle configuración</a></li>
